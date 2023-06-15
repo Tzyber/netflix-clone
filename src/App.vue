7adcde90c6e3cb822ed321585bb0bc4b84687f90
Tzyber <134324777+Tzyber@users.noreply.github.com>
2023-06-15 22:20:58 +0200

    netflix-featu-docunite

---
+++ src/App.vue
@@ -0,0 +1,74 @@
+<script setup lang="ts">
+import {onMounted, ref} from "vue";
+import Magnify from 'vue-material-design-icons/Magnify.vue';
+import HomeOutline from 'vue-material-design-icons/HomeOutline.vue';
+import TrendingUp from 'vue-material-design-icons/TrendingUp.vue';
+import Television from 'vue-material-design-icons/Television.vue';
+import MovieOutline from 'vue-material-design-icons/MovieOutline.vue';
+import Plus from 'vue-material-design-icons/Plus.vue';
+import ChevronLeft from 'vue-material-design-icons/ChevronLeft.vue';
+import movies from './movies.json'
+
+import {useMovieStore} from './stores/movie'
+import {storeToRefs} from 'pinia';
+import MovieDetails from "@/components/MovieDetails.vue";
+import VideoCarousel from "@/components/VideoCarousel.vue";
+
+const useMovie = useMovieStore()
+const {movie, showFullVideo} = storeToRefs(useMovie)
+
+onMounted(() => {
+  setTimeout(() => movie.value = movies[0][0], 100)
+})
+
+</script>
+
+<template>
+  <div class="fixed w-full h-screen bg-black">
+    <div v-if="!showFullVideo" id="SideNav" class="flex z-40 items-center w-[120px] h-screen bg-black relative">
+      <img alt="logo" class="absolute top-0 w-[60px] mt-10 ml-10" src="./assets/docunite_logo_white_350%20(1).png">
+      <div>
+        <div class="py-2 mx-10 my-6">
+          <Magnify fillColor="#FFFFFF" :size="40" class="cursor-pointer"/>
+        </div>
+        <div class="py-2 mx-10 my-6 border-b-4 border-b-red-500">
+          <HomeOutline fillColor="#FFFFFF" :size="40" class="cursor-pointer"/>
+        </div>
+        <div class="py-2 mx-10 my-6">
+          <TrendingUp fillColor="#FFFFFF" :size="40" class="cursor-pointer"/>
+        </div>
+        <div class="py-2 mx-10 my-6">
+          <Television fillColor="#FFFFFF"  :size="40" class="cursor-pointer"/>
+        </div>
+        <div class="py-2 mx-10 my-6">
+          <MovieOutline fillColor="#FFFFFF" :size="40" class="cursor-pointer"/>
+        </div>
+        <div class="py-2 mx-10 my-6">
+          <Plus fillColor="#FFFFFF" :size="40" class="cursor-pointer"/>
+        </div>
+      </div>
+    </div>
+    <div v-if="!showFullVideo">
+      <div class="fixed flex z-20 top-0 right-0 w-full h-[50%] bg-black pl-[120px] bg-clip-border">
+        <div class="absolute z-30 h-[800px] left-[120px] w-[77%] right-0 top-0 bg-gradient-to-r from-black via-black" />
+        <MovieDetails v-if="movie" :movie="movie"/>
+        <img
+            v-if="movie"
+            src="/videos/Catch%20the%20Asset.jpg"
+
+            class="absolute z-0 h-[800px] right-0 top-0"
+        />
+      </div>
+      <div class="fixed z-30 bottom-0 right-0 w-full h-[55%] pl-[120px] overflow-y-auto">
+        <VideoCarousel class="pb-14 üt-14" category="Popular Movies " :movies="movies[0]"/>
+        <VideoCarousel class="pb-14 üt-14" category="Horror Movies " :movies="movies[1]"/>
+        <VideoCarousel class="pb-14 üt-14" category="Featured Movies " :movies="movies[2]"/>
+      </div>
+    </div>
+    <div class="absolute z-20 h-[70%] left-[120px] w-[100%] right-0 bottom-0 bg-gradient-to-t from-black via-black" />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/MovieDetails.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+defineProps({movie: Object})
+</script>
+
+<template>
+<div v-if="movie" class="z-40 text-white w-[53%] pt-20">
+  <span>{{movie.name}}</span>
+  <div class="flex items-center text-lg pt-4">
+    <span>{{movie.year}}</span>
+    <div class="relative">
+    <span class="absolute left-[2px] -top-[12px] text-[40px]">.</span>
+      <span class="pl-4">{{ movie.genre.one}}</span>
+    </div>
+    <div class="relative">
+      <span class="absolute left-[2px] -top-[12px] text-[40px]">.</span>
+      <span class="pl-4">{{ movie.genre.two}}</span>
+    </div>
+    <div class="relative">
+      <span class="absolute left-[2px] -top-[12px] text-[40px]">.</span>
+      <span class="pl-4">{{ movie.genre.three}}</span>
+    </div>
+  </div>
+  <div class="text-2xl pt-6">
+    <span>{{ movie.description }}</span>
+  </div>
+</div>
+
+</template>
+
+<style scoped>
+
+</style>
